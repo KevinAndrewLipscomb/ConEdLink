4dd934e167633cb3b8711d9c45a68b602627b978
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-26 16:38:47 -0500

    Converted work script from CMD to PS1.  Added .gitignore.

--- work.bat
+++
@@ -1,7 +0,0 @@
-REM
-REM $Id: work.bat 5486 2014-11-22 22:37:03Z kevinanlipscomb $
-REM
-cd "C:\Inetpub\wwwroot\ConEdLink"
-start /max explorer /e,/select,C:\Inetpub\wwwroot\ConEdLink\.svn
-start /max ConEdLink.sln
-IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
